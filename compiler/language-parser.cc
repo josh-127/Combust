b3f05bbbfe6a1dc663c6367aedcfc8660ba6b823
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-03-16 05:14:13 -0500

    Add postfix expression parser

--- compiler/language-parser.cc
+++ compiler/language-parser.cc
@@ -2,7 +2,7 @@
 #include "backtracking-lexer.hh"
 #include "syntax.hh"
 
-static Rc<Expression> ParsePrimaryExpression(Rc<BacktrackingLexer> l) {
+static Rc<PrimaryExpression> ParsePrimaryExpression(Rc<BacktrackingLexer> l) {
     BacktrackingLexer::Marker marker{ l->Mark() };
 
     Rc<SyntaxToken> token{ l->ReadToken() };
@@ -22,9 +22,65 @@ static Rc<Expression> ParsePrimaryExpression(Rc<BacktrackingLexer> l) {
 #endif
 
     l->Backtrack(marker);
+    return Rc<PrimaryExpression>{ };
+}
+
+static Rc<Expression> ParsePostfixExpression(Rc<BacktrackingLexer> l) {
+    BacktrackingLexer::Marker marker{ l->Mark() };
+
+    Rc<PrimaryExpression> expression{ ParsePrimaryExpression(l) };
+
+    if (expression) {
+        marker = l->Mark();
+
+        SyntaxNodeVector children{ };
+        children.push_back(expression);
+
+        Rc<SyntaxToken> firstToken{ l->ReadToken() };
+        bool hasPostfix{ false };
+
+        if (IsSyntaxNode<LBracketSymbol>(firstToken)) {
+            Rc<Expression> expression{ ParseExpression(l) };
+            if (expression) {
+                Rc<SyntaxToken> secondToken{ l->ReadToken() };
+                if (IsSyntaxNode<RBracketSymbol>(secondToken)) {
+                    children.push_back(firstToken);
+                    children.push_back(expression);
+                    children.push_back(secondToken);
+                    hasPostfix = true;
+                }
+            }
+        }
+        else if (IsSyntaxNode<DotSymbol>(firstToken)
+            || IsSyntaxNode<MinusGtSymbol>(firstToken))
+        {
+            Rc<SyntaxToken> identifier{ l->ReadToken() };
+            if (IsSyntaxNode<IdentifierToken>(identifier)) {
+                children.push_back(firstToken);
+                children.push_back(identifier);
+                hasPostfix = true;
+            }
+        }
+        else if (IsSyntaxNode<PlusPlusSymbol>(firstToken)
+            || IsSyntaxNode<MinusMinusSymbol>(firstToken))
+        {
+            children.push_back(firstToken);
+            hasPostfix = true;
+        }
+
+        if (!hasPostfix) {
+            l->Backtrack(marker);
+        }
+
+        Rc<PostfixExpression> result{ NewObj<PostfixExpression>() };
+        result->SetChildren(children);
+        return result;
+    }
+
+    l->Backtrack(marker);
     return Rc<Expression>{ };
 }
 
 Rc<Expression> ParseExpression(Rc<BacktrackingLexer> lexer) {
-    return ParsePrimaryExpression(lexer);
+    return ParsePostfixExpression(lexer);
 }
