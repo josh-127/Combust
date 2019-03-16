36d50478591da2806d785e25b3c81f57dec9dd4d
josh-65536 <40677877+josh-65536@users.noreply.github.com>
2019-03-16 01:40:57 -0500

    Add IsAnySyntaxToken<T, Types...>

--- compiler/syntax.hh
+++ compiler/syntax.hh
@@ -233,4 +233,9 @@ template<typename T>
     return visitorFunction.GetResult();
 }
 
+template<typename T, typename... Types>
+[[nodiscard]] inline bool IsAnySyntaxNode(Rc<SyntaxToken> token) {
+    return IsSyntaxNode<T>(token) || IsAnySyntaxNode<Types...>(token);
+}
+
 #endif
